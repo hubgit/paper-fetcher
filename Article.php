f5b88da00f0e7ea02c1bbf6074ff792fdbc76f7f
Alf Eaton <alf@hubmed.org>
2014-04-03 11:58:32 +0100

    improve rel2abs

--- Article.php
+++ Article.php
@@ -268,26 +268,29 @@ class Article {
     /* return if already absolute URL */
     if (parse_url($rel, PHP_URL_SCHEME) != '') return $rel;
 
+    /* remove existing fragments and query strings from the base URL */
+    $base = preg_replace('/[#\?].*/', '', $base);
+
     /* queries and anchors */
     if ($rel[0] == '#' || $rel[0] == '?') return $base . $rel;
 
-    /* parse base URL and convert to local variables: $scheme, $host, $path */
-    extract(parse_url($base));
+    /* parse base URL */
+    $parts = parse_url($base);
 
     /* remove non-directory element from path */
-    $path = preg_replace('#/[^/]*$#', '', $path);
+    $path = preg_replace('#/[^/]*$#', '', $parts['path']);
 
     /* destroy path if relative url points to root */
     if ($rel[0] == '/') $path = '';
 
     /* dirty absolute URL */
-    $abs = $host . $path . '/' . $rel;
+    $abs = $parts['host'] . $path . '/' . $rel;
 
     /* replace '//' or '/./' or '/foo/../' with '/' */
     $re = array('#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#');
     for($n = 1; $n > 0; $abs = preg_replace($re, '/', $abs, -1, $n)) {}
 
     /* absolute URL is ready! */
-    return $scheme . '://' . $abs;
+    return $parts['scheme'] . '://' . $abs;
   }
 }
